ac5e9c6da2ba89035af5138420752ca93efbfc4a
Yumshot <Clearlyio@protonmail.com>
2023-03-06 17:21:09 -0800

    Theme Updates

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
 
-const config: DocsThemeConfig = {
-   head: (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="Halcyon" />
-      <meta property="og:description" content="Discover Adventure" />
-    </>
-  ),
-  logo: <span>My Project</span>,
+export default {
+  banner: {
+    key: "0.0.1-Devlog",
+    text: <p>🎉 Halcyon is Currently in Development! 🎉</p>,
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – Halcyon",
+    };
+  },
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/Yumshot/Halcyon_Overview",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Halcyon Documentation',
-  },
-}
+  logo: (
+    <svg width="24" height="24" viewBox="0 0 256 256">
+      <path
+        fill="currentColor"
+        d="m231.9 169.8l-94.8 65.6a15.7 15.7 0 0 1-18.2 0l-94.8-65.6a16.1 16.1 0 0 1-6.4-17.3L45 50a12 12 0 0 1 22.9-1.1L88.5 104h79l20.6-55.1A12 12 0 0 1 211 50l27.3 102.5a16.1 16.1 0 0 1-6.4 17.3Z"
+      ></path>
+    </svg>
+  ),
+};
 
-export default config
+// const config: DocsThemeConfig = {
+//    head: "Halcyon",
+//    logo: <svg width="24" height="24" viewBox="0 0 256 256"><path fill="currentColor" d="m231.9 169.8l-94.8 65.6a15.7 15.7 0 0 1-18.2 0l-94.8-65.6a16.1 16.1 0 0 1-6.4-17.3L45 50a12 12 0 0 1 22.9-1.1L88.5 104h79l20.6-55.1A12 12 0 0 1 211 50l27.3 102.5a16.1 16.1 0 0 1-6.4 17.3Z"></path></svg>,
+//   chat: {
+//     link: 'https://discord.com',
+//   },
+//   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+//   footer: {
+//     text: 'Halcyon Documentation',
+//   },
+// }
+
+// export default config
